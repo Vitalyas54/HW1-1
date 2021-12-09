3333dc52fd010aa0d25978a3b499cc22e63501a8
Vitaliy Bozhko <raptor1111@list.ru>
2021-12-09 19:50:03 +0700

    Java project added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balanceSum=2_000_000_000;
+        int balanceRefilling=500_000_000;
+        int totalBalance=balanceSum+balanceRefilling;
+        System.out.println("Итоговый баланс " + totalBalance + " руб.");
+    }
+}
